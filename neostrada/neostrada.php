586872f3b7670c095cd874325234321ad07df7be
Stefan van Scherpenseel <stefan@enlogic.nl>
2019-03-19 14:15:20 +0100

    Removed lines which forcefully enabled error reporting

--- neostrada/neostrada.php
+++ neostrada/neostrada.php
@@ -1,8 +1,5 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 if (!defined('WHMCS')) {
     die('This file cannot be accessed directly');
 }
@@ -310,15 +307,6 @@ function neostrada_SaveContactDetails($params)
     }
 
     return $rc;
-}
-
-/**
- * Settings for domain suggestions.
- *
- * @return array
- */
-function neostrada_DomainSuggestionOptions() {
-    return [];
 }
 
 /**
